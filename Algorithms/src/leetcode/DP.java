3624ec0d694e96889264d81a6911280e04063275
joker_xjh <1781934063@qq.com>
2017-12-07 15:50:18 +0800

    Decode Ways

--- Algorithms/src/leetcode/DP.java
+++ Algorithms/src/leetcode/DP.java
@@ -276,6 +276,81 @@ public class DP {
     }
     
     
+    public int numDecodings(String s) {
+        if(s == null || s.length() == 0)
+        	return 0;
+        if(s.charAt(0) == '0'){
+            return 0;
+        }
+        long[] dp = new long[s.length()+1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '*' ? 9 : 1;
+        for(int i=2; i<=s.length(); i++) {
+        	char first = s.charAt(i-2);
+        	char second = s.charAt(i-1);
+        	if(second == '*') {
+        		dp[i] += 9 * dp[i-1];
+        	}
+        	else if(second > '0'){
+        		dp[i] += dp[i-1];
+        	}
+        	
+        	if(first == '*') {
+        		if(second == '*')
+        			dp[i] += dp[i-2] * 15;
+        		else if(second <= '6')
+        			dp[i] += dp[i-2] * 2;
+        		else 
+        			dp[i] += dp[i-2];
+        	}
+        	else if(first == '1') {
+        		if(second == '*')
+        			dp[i] += dp[i-2] * 9;
+        		else
+        			dp[i] += dp[i-2];
+        	}
+        	else if(first == '2') {
+        		if(second == '*')
+        			dp[i] += 6 * dp[i-2];
+        		else if(second <= '6')
+        			dp[i] += dp[i-2];
+        	}
+        	
+        	dp[i] %= 1000000007;
+        }
+    	
+    	return (int) dp[s.length()];
+    }
+    
+    
+    public int numDecodings1(String s) {
+    	if(s == null || s.length() == 0)
+    		return 0;
+    	if(s.charAt(0) == '0')
+    		return 0;
+    	int n = s.length();
+    	int[] dp = new int[n+1];
+    	dp[0] = 1;
+    	dp[1] = 1;
+    	char[] array = s.toCharArray();
+    	for(int i=2; i<=n; i++) {
+    		char first = array[i-2];
+    		char second = array[i-1];
+    		if(second > '0')
+    			dp[i] += dp[i-1];
+    		if(first == '1') {
+    			dp[i] += dp[i-2];
+    		}
+    		else if(first == '2') {
+    			if(second <= '6')
+    				dp[i] += dp[i-2];
+    		}
+    		
+    	}
+    	
+    	return dp[n];
+    }
+    
     
     
 	
