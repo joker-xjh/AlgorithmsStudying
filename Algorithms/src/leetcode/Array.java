ea51f27216393c9c698b7b3e95eb4ca36c49044b
joker_xjh <1781934063@qq.com>
2017-12-12 14:23:58 +0800

    Find Smallest Letter Greater Than Target

--- Algorithms/src/leetcode/Array.java
+++ Algorithms/src/leetcode/Array.java
@@ -1169,6 +1169,26 @@ public class Array {
     }
     
     
+    public char nextGreatestLetter(char[] letters, char target) {
+    	if(target >= letters[letters.length-1])
+    		return letters[0];
+    	target++;
+        int left = 0, right = letters.length-1;
+        while(left <= right) {
+        	int mid = left + (right - left) / 2;
+        	if(letters[mid] == target) {
+        		left = mid;
+        		break;
+        	}
+        	else if(letters[mid] < target)
+        		left = mid + 1;
+        	else 
+        		right = mid - 1;
+        }
+    	return letters[left];
+    }
+    
+    
     
 
 	public static void main(String[] args) {
