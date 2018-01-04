7b1479ef3daef7a042cbf9bb5a6ae6b8a0f7d005
joker_xjh <1781934063@qq.com>
2018-01-04 16:06:52 +0800

    Heaters

--- Algorithms/src/leetcode/Array.java
+++ Algorithms/src/leetcode/Array.java
@@ -1343,6 +1343,22 @@ public class Array {
     }
     
     
+    
+    public int findRadius(int[] houses, int[] heaters) {
+        Arrays.sort(heaters);
+        int answer = -1;
+        for(int house : houses) {
+        	int index = Arrays.binarySearch(heaters, house);
+        	if(index < 0)
+        		index = -(index+1);
+        	int dis1 = index - 1 >= 0 ? house - heaters[index-1] : Integer.MAX_VALUE;
+        	int dis2 = index < heaters.length ? heaters[index] - house : Integer.MAX_VALUE;
+        	answer = Math.max(answer, Math.min(dis1, dis2));
+        }
+        return answer;
+    }
+    
+    
 
 
 	public static void main(String[] args) {
