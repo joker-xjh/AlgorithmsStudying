af273f25281696c1307ba9408feb94369004b429
joker_xjh <1781934063@qq.com>
2018-01-17 13:21:32 +0800

    Mini Parser

--- Algorithms/src/leetcode/string.java
+++ Algorithms/src/leetcode/string.java
@@ -1510,9 +1510,86 @@ public class string {
     }
     
     
+      class NestedInteger {
+    	      // Constructor initializes an empty nested list.
+    	      public NestedInteger() {
+			}
+    	 
+    	      // Constructor initializes a single integer.
+    	      public  NestedInteger(int value) {
+			}
+    	 
+    	      // @return true if this NestedInteger holds a single integer, rather than a nested list.
+    	      public boolean isInteger() {
+				return false;
+			}
+    	 
+    	      // @return the single integer that this NestedInteger holds, if it holds a single integer
+    	      // Return null if this NestedInteger holds a nested list
+    	      public Integer getInteger() {
+				return null;
+			}
+    	 
+    	      // Set this NestedInteger to hold a single integer.
+    	      public void setInteger(int value) {
+			}
+    	 
+    	      // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+    	      public void add(NestedInteger ni) {
+			}
+    	 
+    	      // @return the nested list that this NestedInteger holds, if it holds a nested list
+    	      // Return null if this NestedInteger holds a single integer
+    	      public List<NestedInteger> getList() {
+				return null;
+			}
+    	  }
     
-    
-    
+     
+     public NestedInteger deserialize(String s) {
+    	 Stack<NestedInteger> stack = new Stack<>();
+    	 NestedInteger cur = null;
+    	 char[] array = s.toCharArray();
+    	 StringBuilder sb = new StringBuilder();
+    	 for(int i=0; i<array.length; i++) {
+    		 char c = array[i];
+    		 if((c >= '0' && c <= '9') || c == '-') {
+    			 sb.append(c);
+    		 }
+    		 else if(c == '[') {
+    			 NestedInteger nestedInteger = new NestedInteger();
+    	    	 stack.push(nestedInteger);
+    	    	 if(cur != null) {
+    	    		 cur.add(nestedInteger);
+    	    	 }
+    	    	 cur = nestedInteger;
+    		 }
+    		 else if(c == ']') {
+    			 if(sb.length() > 0) {
+    				 cur.add(new NestedInteger(Integer.parseInt(sb.toString())));
+    				 sb = new StringBuilder();
+    			 }
+    			 stack.pop();
+    			 if(!stack.isEmpty()) {
+    				 cur = stack.peek();
+    			 }
+    		 }
+    		 else if(c == ',') {
+    			 if(sb.length() > 0) {
+    				 cur.add(new NestedInteger(Integer.parseInt(sb.toString())));
+    				 sb = new StringBuilder();
+    			 }
+    		 }
+    	 }
+    	 if(sb.length() > 0) {
+			 if(cur == null) {
+				 cur = new NestedInteger(Integer.parseInt(sb.toString()));
+			 }
+			 
+    	 }
+    	 
+    	 return cur;
+     }
     
    
     
