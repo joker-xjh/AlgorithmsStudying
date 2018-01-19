b7217d5060c4a003ee5cdd7d5759fe6ee8a4de66
joker_xjh <1781934063@qq.com>
2018-01-19 14:10:15 +0800

    H-Index

--- Algorithms/src/leetcode/Array.java
+++ Algorithms/src/leetcode/Array.java
@@ -1500,6 +1500,20 @@ public class Array {
      }
      
      
+     public int hIndex(int[] citations) {
+         int h = 1;
+         Arrays.sort(citations);
+         for(int i=citations.length-1; i >=0; i--) {
+        	 if(citations[i] >= h)
+        		 h++;
+        	 else
+        		 break;
+         }
+         h--;
+         return h;
+     }
+     
+     
 
 
 	public static void main(String[] args) {
