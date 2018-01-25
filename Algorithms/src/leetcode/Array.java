2ba2d2c7b67be9acc6fea0afb12e6b653ee24996
joker_xjh <1781934063@qq.com>
2018-01-25 17:16:17 +0800

    Trapping Rain Water II

--- Algorithms/src/leetcode/Array.java
+++ Algorithms/src/leetcode/Array.java
@@ -1697,6 +1697,66 @@ public class Array {
      }
      
      
+     
+     class Cell implements Comparable<Cell> {
+    	int row;
+    	int col;
+    	int height;
+    	public Cell(int row, int col, int height) {
+			this.row = row;
+			this.col = col;
+			this.height = height;
+		}
+		@Override
+		public int compareTo(Cell o) {
+			return this.height - o.height;
+		}
+    	 
+     }
+     
+     public int trapRainWater(int[][] heightMap) {
+         int answers = 0;
+         if(heightMap == null || heightMap.length == 0 || heightMap[0].length == 0)
+        	 return answers;
+         int m = heightMap.length, n = heightMap[0].length;
+         boolean[][] visited = new boolean[m][n];
+         PriorityQueue<Cell> queue = new PriorityQueue<>();
+         
+         for(int i=0; i<m; i++) {
+        	 visited[i][0] = true;
+        	 visited[i][n-1] = true;
+        	 queue.offer(new Cell(i, 0, heightMap[i][0]));
+        	 queue.offer(new Cell(i, n-1, heightMap[i][n-1]));
+         }
+         for(int i=0; i<n; i++) {
+        	 visited[0][i] =true;
+        	 visited[m-1][i] = true;
+        	 queue.offer(new Cell(0, i, heightMap[0][i]));
+        	 queue.offer(new Cell(m-1, i, heightMap[m-1][i]));
+         }
+         int[][] dirs = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+         while(!queue.isEmpty()) {
+        	 Cell cell = queue.poll();
+        	 for(int[] dir : dirs) {
+        		 int row = cell.row + dir[0];
+        		 int col = cell.col + dir[1];
+        		 if(row < 0 || row >= m || col < 0 || col >= n || visited[row][col])
+        			 continue;
+        		 visited[row][col] = true;	
+        		 answers += Math.max(0, cell.height - heightMap[row][col]);
+        		 queue.offer(new Cell(row, col, Math.max(heightMap[row][col], cell.height)));
+        	 }
+         }
+         
+         return answers;
+     }
+     
+     
+     
+     
+     
+     
+     
 
 
 	public static void main(String[] args) {
