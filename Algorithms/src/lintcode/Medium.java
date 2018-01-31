a147e5049622c6ca3120c8cd8c7fdc68cb79c6b3
joker_xjh <1781934063@qq.com>
2018-01-31 21:25:32 +0800

    统计数字

---
+++ Algorithms/src/lintcode/Medium.java
@@ -0,0 +1,35 @@
+package lintcode;
+
+public class Medium {
+	
+	public int digitCounts(int k, int n) {  
+		int count = 0;
+		if(n <= 0)
+			return 0;
+		int left_part = n, right_part = 0, power = 1;
+		while(left_part > 0) {
+			int digit = left_part % 10;
+			left_part /= 10;
+			
+			if(digit < k) {
+				count += left_part * power;
+			}
+			else if(digit == k) {
+				count += left_part * power + right_part + 1;
+			}
+			else {
+				count += (left_part + 1) * power;
+			}
+			
+			if(k == 0 && power > 1)
+				count -= power;
+			
+			right_part = digit * power + right_part;
+			power *= 10;
+		}
+		
+		
+		return count;
+	}
+
+}
