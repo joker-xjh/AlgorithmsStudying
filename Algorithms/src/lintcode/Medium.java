3142460be1e64f6f7d54177dce153997ceda4709
joker_xjh <1781934063@qq.com>
2018-02-01 14:20:55 +0800

    丑数2

--- Algorithms/src/lintcode/Medium.java
+++ Algorithms/src/lintcode/Medium.java
@@ -1,9 +1,15 @@
 package lintcode;
 
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
 public class Medium {
 	
 	public int digitCounts(int k, int n) {  
 		int count = 0;
+		if(n == 0 && k == 0)
+			return 1;
 		if(n <= 0)
 			return 0;
 		int left_part = n, right_part = 0, power = 1;
@@ -31,5 +37,86 @@ public class Medium {
 		
 		return count;
 	}
+	
+	
+	
+	public int nthUglyNumber(int n) {
+        long ugly = 0;
+        PriorityQueue<Long> queue = new PriorityQueue<>();
+        Set<Long> used = new HashSet<>();
+        
+        queue.offer(1L);
+        used.add(1L);
+        
+        while(n > 0) {
+        	ugly = queue.poll();
+        	long one = ugly * 2;
+        	if(!used.contains(one) && one > 0) {
+        		used.add(one);
+        		queue.offer(one);
+        	}
+        	long two = ugly * 3;
+        	if(!used.contains(two) && two > 0) {
+        		used.add(two);
+        		queue.offer(two);
+        	}
+        	long three = ugly * 5;
+        	if(!used.contains(three) && three > 0) {
+        		used.add(three);
+        		queue.offer(three);
+        	}
+        	n--;
+        }
+        
+        return (int) ugly;
+    }
+	
+	
+	public int nthUglyNumber2(int n) {
+        long ugly = 0;
+        long pre = 0;
+        PriorityQueue<Long> queue = new PriorityQueue<>();
+        
+        queue.offer(1L);
+        
+        while(n > 0) {
+        	ugly = queue.poll();
+        	if(pre == ugly)
+        		continue;
+        	long one = ugly * 2;
+        	if(one > 0) {
+        		queue.offer(one);
+        	}
+        	long two = ugly * 3;
+        	if( two > 0) {
+        		queue.offer(two);
+        	}
+        	long three = ugly * 5;
+        	if(three > 0) {
+        		queue.offer(three);
+        	}
+        	n--;
+        	pre = ugly;
+        }
+        
+        return (int) ugly;
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
