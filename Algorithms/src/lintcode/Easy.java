e84a67c3072574981f16e72b3baebd099596f251
joker_xjh <1781934063@qq.com>
2018-02-02 19:01:47 +0800

    practice

--- Algorithms/src/lintcode/Easy.java
+++ Algorithms/src/lintcode/Easy.java
@@ -1,5 +1,8 @@
 package lintcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Easy {
 	
 	public int aplusb(int a, int b) {
@@ -38,5 +41,61 @@ public class Easy {
         
         return array;
     }
+	
+	
+	public void rotateString(char[] str, int offset) {
+		if(str == null || str.length < 2)
+			return;
+		offset = offset % str.length;
+        offset = str.length - offset;
+        flipString(str, offset, str.length);
+        flipString(str, 0, offset);
+        flipString(str, 0, str.length);
+    }
+	
+	private void flipString(char[] str, int left, int right) {
+		int mid = left + (right - left) / 2;
+		for(int i=left; i<mid; i++) {
+			char c = str[i];
+			str[i] = str[right - 1 - i + left];
+			str[right - 1 - i + left] = c;
+		}
+	}
+	
+	public List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>();
+        String fizz = "fizz";
+        String buzz = "buzz";
+        for(int i=1; i<=n; i++) {
+        	boolean three = i % 3 == 0;
+        	boolean five = i % 5 == 0;
+        	if(three && five)
+        		list.add(fizz + " "+ buzz);
+        	else if(three)
+        		list.add(fizz);
+        	else if(five)
+        		list.add(buzz);
+        	else
+        		list.add(String.valueOf(i));
+        }
+        
+        return list;
+    }
+	
+	public int strStr(String source, String target) {
+        if(source == null || target == null)
+        	return -1;
+        return source.indexOf(target);
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
--- Algorithms/src/lintcode/Medium.java
+++ Algorithms/src/lintcode/Medium.java
@@ -1,8 +1,15 @@
 package lintcode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Set;
+import java.util.Stack;
 
 public class Medium {
 	
@@ -134,14 +141,109 @@ public class Medium {
 	}
 	
 	
+	class TreeNode {
+	      public int val;
+	      public TreeNode left, right;
+	      public TreeNode(int val) {
+	          this.val = val;
+	          this.left = this.right = null;
+          }
+	}
+	
+	Map<String, TreeNode> serializeMap = new HashMap<>();
+	public String serialize(TreeNode root) {
+		if(root == null) {
+			serializeMap.put("null", root);
+			return "null";
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.offer(root);
+		while(!queue.isEmpty()) {
+			int size = queue.size();
+			for(int i=0; i<size; i++) {
+				TreeNode node = queue.poll();
+				sb.append(node.val).append(',');
+				if(node.left != null)
+					queue.offer(node.left);
+				else 
+					sb.append('#').append(',');
+				if(node.right != null)
+					queue.offer(node.right);
+				else 
+					sb.append('#').append(',');
+			}
+		}
+		String key = sb.toString();
+		serializeMap.put(key, root);
+		return key;
+    }
+	
+	 public TreeNode deserialize(String data) {
+		 return  serializeMap.get(data);
+	 }
 	
 	
+	 public List<Integer> searchRange(TreeNode root, int k1, int k2) {
+	       List<Integer> list = new ArrayList<>();
+	       searchRangeHelp(root, k1, k2, list);
+	       return list;
+	 }
+	 
+	 private void searchRangeHelp(TreeNode node, int k1, int k2, List<Integer> list) {
+		 if(node == null)
+			 return;
+		 searchRangeHelp(node.left, k1, k2, list);
+		 if(node.val >= k1 && node.val <= k2)
+			 list.add(node.val);
+		 searchRangeHelp(node.right, k1, k2, list);
+	 }
 	
-	
-	
-	
-	
-	
+	  class MinStack {
+		  
+		    class Node {
+		    	int val;
+		    	int smallest;
+		    }
+		    Stack<Node> stack;
+		    public MinStack() {
+		        stack = new Stack<>();
+		    }
+
+		    /*
+		     * @param number: An integer
+		     * @return: nothing
+		     */
+		    public void push(int number) {
+		    	Node node = new Node();
+	        	node.val = number;
+		        if(stack.isEmpty()) {
+		        	node.smallest = number;
+		        }
+		        else {
+		        	if(number < stack.peek().smallest)
+		        		node.smallest = number;
+		        	else
+		        		node.smallest = stack.peek().smallest;
+		        }
+	        	stack.push(node);
+		    }
+
+		    /*
+		     * @return: An integer
+		     */
+		    public int pop() {
+		    	return stack.pop().val;
+		    }
+
+		    /*
+		     * @return: An integer
+		     */
+		    public int min() {
+		        return stack.peek().smallest;
+		    }
+		}
 	
 	
 	
