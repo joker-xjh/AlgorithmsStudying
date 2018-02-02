84340f4fbdcd5edb4cba65273611cd5d8c40c1fb
joker_xjh <1781934063@qq.com>
2018-02-02 20:36:29 +0800

    Recursive Digit Sum

--- Algorithms/src/hackerrank/Medium.java
+++ Algorithms/src/hackerrank/Medium.java
@@ -263,7 +263,22 @@ public class Medium {
 		}
 	}
 	
-	
+	static int digitSum(String n, int k) {
+        long sum = 0;
+        for(char c : n.toCharArray())
+        	sum += c - '0';
+		sum *= k;
+		while(sum >= 10) {
+			long temp = 0;
+			while(sum != 0) {
+				temp += sum %10;
+				sum /= 10;
+			}
+			sum = temp;
+		}
+        
+		return (int) sum;
+    }
 	
 	
 	
