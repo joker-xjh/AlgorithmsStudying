67babd4cadd5cc62c33c557d821e9ac0611493e2
joker_xjh <1781934063@qq.com>
2018-02-10 09:35:17 +0800

    similarPair

--- Algorithms/src/hackerrank/Hard.java
+++ Algorithms/src/hackerrank/Hard.java
@@ -2,8 +2,12 @@ package hackerrank;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Hard {
 	
@@ -45,5 +49,94 @@ public class Hard {
 		memorization.put(key, false);
 		return false;
 	}
+	
+	
+	static void similarPair(int n, int[][] edges, int k) {
+		boolean[] visited = new boolean[n+1];
+		Map<Integer, Set<Integer>> tree = new HashMap<>();
+		for(int[] edge : edges) {
+			int v = edge[0], p = edge[1];
+			if(!tree.containsKey(v))
+				tree.put(v, new HashSet<>());
+			tree.get(v).add(p);
+		}
+		TreeSet<Integer> values = new TreeSet<>();
+		visited[edges[0][0]] = true;
+		int count = similarPairHelp(tree, edges[0][0], visited, values, k);
+		System.out.println(count);
+	}
+	
+	static int similarPairHelp(Map<Integer, Set<Integer>> tree, int v, boolean[] visited, TreeSet<Integer> values, int k) {
+		int count = 0;
+		count += similarPairCount(values, v, k, visited.length-1);
+		Set<Integer> adj = tree.get(v);
+		if(adj != null && adj.size() > 0) {
+			values.add(v);
+			for(int p : tree.get(v)) {
+				if(visited[p])
+					continue;
+				visited[p] = true;
+				count += similarPairHelp(tree, p, visited, values, k);
+			}
+			values.remove(v);
+		}
+		return count;
+	}
+	
+	
+	
+	static int similarPairCount(TreeSet<Integer> values, int v, int k, int n) {
+		if(k == n)
+			return values.size();
+		int count = 0;
+		int temp = v;
+		while(true) {
+			Integer pre = values.lower(temp);
+			if(pre == null || Math.abs(pre - v) > k)
+				break;
+			//System.out.println("["+pre+", "+v+"]");
+			count++;
+			temp = pre;
+		}
+		temp = v;
+		while(true) {
+			Integer next = values.higher(temp);
+			if(next == null || Math.abs(next - v) > k)
+				break;
+			//System.out.println("["+next+", "+v+"]");
+			count++;
+			temp = next;
+		}
+		return count;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int k = scanner.nextInt();
+		int[][] edges = new int[n-1][2];
+		for(int i=0; i<n-1; i++) {
+			int[] array = new int[2];
+			array[0] = scanner.nextInt();
+			array[1] = scanner.nextInt();
+			edges[i] = array;
+		}
+		similarPair(n, edges, k);
+		scanner.close();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
