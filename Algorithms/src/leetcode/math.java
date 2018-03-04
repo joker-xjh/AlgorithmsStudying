cce08bafc1569eb1fa133ffc03049eba71add681
joker_xjh <1781934063@qq.com>
2018-03-04 08:59:21 +0800

    Water and Jug Problem

--- Algorithms/src/leetcode/math.java
+++ Algorithms/src/leetcode/math.java
@@ -176,7 +176,21 @@ public class math {
     }
     
     
-    
+    public boolean canMeasureWater(int x, int y, int z) {
+        if(x + y < z) return false;
+        if( x == z || y == z || x + y == z ) 
+        	return true;        
+        return z%GCD(x, y) == 0;
+    }
+
+    public int GCD(int a, int b){
+        while(b != 0 ){
+            int temp = b;
+            b = a%b;
+            a = temp;
+        }
+        return a;
+    }
     
     
     
