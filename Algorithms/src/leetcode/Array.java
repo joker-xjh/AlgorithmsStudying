bdc1ecc27aef0500b1f3cc98bd6365dbf4063c5c
joker_xjh <1781934063@qq.com>
2018-03-05 10:01:56 +0800

    Number of Subarrays with Bounded Maximum

--- Algorithms/src/leetcode/Array.java
+++ Algorithms/src/leetcode/Array.java
@@ -2715,7 +2715,22 @@ public class Array {
      }
      
      
-     
+     public int numSubarrayBoundedMax(int[] A, int L, int R) {
+         int j=0,count=0,res=0;
+         for(int i=0;i<A.length;i++){
+             if(A[i]>=L && A[i]<=R){
+                 res+=i-j+1;count=i-j+1;
+             }
+             else if(A[i]<L){
+                 res+=count;
+             }
+             else{
+                 j=i+1;
+                 count=0;
+             }
+         }
+         return res;
+     }
      
      
      
