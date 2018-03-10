0acfb172dada9536e272a5eeb97b63648eca53a7
joker_xjh <1781934063@qq.com>
2018-03-10 14:03:42 +0800

    Course Schedule III

--- Algorithms/src/leetcode/Array.java
+++ Algorithms/src/leetcode/Array.java
@@ -2733,7 +2733,18 @@ public class Array {
      }
      
      
-     
+     public int scheduleCourse(int[][] courses) {
+         Arrays.sort(courses, (a,b)->a[1]-b[1]);
+         PriorityQueue<Integer> heap = new PriorityQueue<>((a,b)->b-a);
+         int time = 0;
+         for(int[] course : courses) {
+        	 time += course[0];
+        	 heap.add(course[0]);
+        	 if(time > course[1])
+        		 time -= heap.poll();
+         }
+         return heap.size();
+     }
      
      
      
