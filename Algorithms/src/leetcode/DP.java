dbb3263b2a8439691b55f416544d7b08c453c137
joker_xjh <1781934063@qq.com>
2018-04-06 21:52:41 +0800

    K Inverse Pairs Array

--- Algorithms/src/leetcode/DP.java
+++ Algorithms/src/leetcode/DP.java
@@ -749,6 +749,54 @@ public class DP {
     }
     
     
+    //wrong answer
+    public boolean xorGame(int[] nums) {
+        int n = nums.length;
+        int xor = 0;
+        for(int num : nums)
+        	xor ^= num;
+        boolean[] used = new boolean[n];
+    	return xorGameMiniMax(nums, xor, used);
+    }
+    
+    private boolean xorGameMiniMax(int[] nums, int xor, boolean[] used) {
+    	if(xor == 0)
+    		return true;
+    	for(int i=0; i<nums.length; i++) {
+    		if(used[i])
+    			continue;
+    		used[i] = true;
+    		xor ^= nums[i];
+    		if(!xorGameMiniMax(nums, xor, used))
+    			return true;
+    		xor ^= nums[i];
+    		used[i] = false;
+    	}
+    	return false;
+    }
+    
+    
+    public int kInversePairs(int n, int k) {
+    	int mod = 1000000007;
+        if (k > n*(n-1)/2 || k < 0) return 0;
+        if (k == 0 || k == n*(n-1)/2) return 1;
+        long[][] dp = new long[n+1][k+1];
+        dp[2][0] = 1;
+        dp[2][1] = 1;
+        for (int i = 3; i <= n; i++) {
+            dp[i][0] = 1;
+            for (int j = 1; j <= Math.min(k, i*(i-1)/2); j++) {
+                dp[i][j] = dp[i][j-1] + dp[i-1][j];
+                if (j >= i) dp[i][j] -= dp[i-1][j-i];
+                dp[i][j] = (dp[i][j]+mod) % mod;
+            }
+        }
+        return (int) dp[n][k];
+    }
+    
+    
+    
+    
     
     
     public static void main(String[] args) {
