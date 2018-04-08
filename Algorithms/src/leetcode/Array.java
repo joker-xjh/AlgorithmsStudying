f79c3a811bda77b99ab51d82ebbafd236e91ca41
joker_xjh <1781934063@qq.com>
2018-04-08 15:51:08 +0800

    Patching Array

--- Algorithms/src/leetcode/Array.java
+++ Algorithms/src/leetcode/Array.java
@@ -3055,7 +3055,19 @@ public class Array {
     			 splitArraySameAverageFindB(array, target, lenB, i+1);
      }
      
-     
+     public int minPatches(int[] nums, int n) {
+         int add = 0, i = 0;
+         long miss = 1;
+         while(miss <= n) {
+        	 if(i < nums.length && nums[i] <= miss)
+        		 miss += nums[i++];
+        	 else {
+        		 miss += miss;
+        		 add++;
+        	 }
+         }
+         return add;
+     }
      
      
      
