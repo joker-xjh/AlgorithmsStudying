7bee32530febb142bbb6f2541ec37bf4bd93de0b
joker_xjh <1781934063@qq.com>
2018-04-09 11:09:23 +0800

    Binary Tree Pruning

--- Algorithms/src/leetcode/Tree.java
+++ Algorithms/src/leetcode/Tree.java
@@ -750,7 +750,15 @@ public class Tree {
     }
     
     
-    
+    public TreeNode pruneTree(TreeNode root) {
+        if(root == null)
+        	return null;
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        if(root.left == null && root.right == null && root.val == 0)
+        	return null;
+        return root;
+    }
     
     
     
