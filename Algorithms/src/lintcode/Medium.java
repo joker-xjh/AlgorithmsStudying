6a76687e9b24038e96cffd857b92060833b5e048
joker_xjh <1781934063@qq.com>
2018-04-13 13:18:49 +0800

    不同的子序列

--- Algorithms/src/lintcode/Medium.java
+++ Algorithms/src/lintcode/Medium.java
@@ -1017,6 +1017,27 @@ public class Medium {
 		   return dp[index][m];
 	  }
 	  
+	  public int numDistinct(String S, String T) {
+		  int len1 = S.length(), len2 = T.length();
+		  int[][] dp = new int[len2+1][len1+1];
+		  for(int i=0; i<=len1; i++)
+			  dp[0][i] = 1;
+		  for(int i=1; i<=len2; i++) {
+			  char c2 = T.charAt(i-1);
+			  for(int j=1; j<=len1; j++) {
+				  char c1 = S.charAt(j-1);
+				  if(c1 == c2) {
+					  dp[i][j] += dp[i][j-1] + dp[i-1][j];
+				  }
+				  else {
+					  dp[i][j] += dp[i][j-1];
+				  }
+			  }
+		  }
+		  
+		  return dp[len2][len1];
+	  }
+	  
 	  
 	  
 	  
