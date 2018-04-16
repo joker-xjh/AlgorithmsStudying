093b49d45ec73cb0e79c54427a820658402c8bd1
joker_xjh <1781934063@qq.com>
2018-04-16 10:26:49 +0800

    最大权值和路径

--- Algorithms/src/lintcode/Medium.java
+++ Algorithms/src/lintcode/Medium.java
@@ -1140,7 +1140,24 @@ public class Medium {
 	  }
 	  
 	  
-	  
+	  public int maxWeight(int[][] nums) {
+	        int m = nums.length, n = nums[0].length;
+	        if(m == 0 || n == 0)
+	        	return 0;
+	        int[][] dp = new int[m][n];
+	        dp[0][n-1] = nums[0][n-1];
+	        for(int i=n-2; i>=0; i--)
+	        	dp[0][i] = dp[0][i+1] + nums[0][i];
+	        for(int i=1; i<m; i++)
+	        	dp[i][n-1] = dp[i-1][n-1] + nums[i][n-1];
+	        for(int i=1; i<m; i++) {
+	        	for(int j = n-2; j>=0; j--) {
+	        		dp[i][j] = Math.max(dp[i-1][j], dp[i][j+1]) + nums[i][j];
+	        	}
+	        }
+	        
+	        return dp[m-1][0];
+	  }
 	  
 	  
 	  
