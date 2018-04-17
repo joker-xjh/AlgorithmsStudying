3e850c50d4490eca123d7d154147c37486e50cd6
joker_xjh <1781934063@qq.com>
2018-04-17 14:10:57 +0800

    最长公共子序列

--- Algorithms/src/lintcode/Medium.java
+++ Algorithms/src/lintcode/Medium.java
@@ -1258,7 +1258,24 @@ public class Medium {
 	        return dp;
 	  }
 	  
-	  
+	  public int longestCommonSubsequence(String A, String B) {
+		  int lenA = A.length();
+		  int lenB = B.length();
+		  int[][] dp = new int[lenA+1][lenB+1];
+		  for(int i=1; i<=lenA; i++) {
+			  char c1 = A.charAt(i-1);
+			  for(int j=1; j<=lenB; j++) {
+				  char c2 = B.charAt(j-1);
+				  if(c1 == c2) {
+					  dp[i][j] = dp[i-1][j-1] + 1;
+				  }
+				  else {
+					  dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+				  }
+			  }
+		  }
+		  return dp[lenA][lenB];
+	  }
 	  
 	  
 	  
