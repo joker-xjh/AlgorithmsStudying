43578b4bedaf9f3f0b2d5cd2326f22019ce44384
joker_xjh <1781934063@qq.com>
2018-04-22 10:36:41 +0800

    Integer Break

--- Algorithms/src/leetcode/DP.java
+++ Algorithms/src/leetcode/DP.java
@@ -1385,6 +1385,31 @@ public class DP {
     }
     
     
+    public int integerBreak(int n) {
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 1;
+        for(int i=3; i<=n; i++) {
+        	for(int j=1; j<=i/2; j++) {
+        		dp[i] = Math.max(dp[i], dp[i-j] * dp[j]);
+        		dp[i] = Math.max(dp[i],  j * (i - j));
+        		dp[i] = Math.max(dp[i],  j * dp[i-j]);
+        		dp[i] = Math.max(dp[i],  (i-j) * dp[j]);
+        	}
+        }
+    	
+    	return dp[n];
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     public static void main(String[] args) {
     	
