cc005f55b870b99fdab285fcd2090f54302734fa
joker_xjh <1781934063@qq.com>
2018-04-25 19:06:01 +0800

    Continuous Subarray Sum

--- Algorithms/src/leetcode/DP.java
+++ Algorithms/src/leetcode/DP.java
@@ -1542,6 +1542,53 @@ public class DP {
     }
     
     
+    public boolean checkSubarraySum(int[] nums, int k) {
+    	if(nums.length < 2)
+    		return false;
+    	 if(k == 0) {
+    		 for(int i=1; i<nums.length; i++) {
+    			 if(nums[i] == 0 && nums[i-1] == 0)
+    				 return true;
+    		 }
+    		 return false;
+    	 }
+         if(k == 1)
+             return true;
+    	return checkSubarraySumMemo(nums, k, nums.length, new Boolean[nums.length]);
+    }
+    
+    private boolean checkSubarraySumMemo(int[] nums, int k, int n, Boolean[] dp) {
+    	if(n < 2)
+    		return false;
+    	if(dp[n] != null)
+    		return dp[n];
+    	int temp = nums[n-1];
+    	boolean b = false;
+    	for(int i=n-1; i>=1; i--) {
+    		temp += nums[i-1];
+    		if(temp % k == 0) {
+    			b = true;
+    			break;
+    		}
+    	}
+    	if(!b)
+    		b = checkSubarraySumMemo(nums, k, n-1, dp);
+    	dp[n] = b;
+    	return dp[n];
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
