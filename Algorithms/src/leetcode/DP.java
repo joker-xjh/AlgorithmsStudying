ce95394f9bfa92de59332980833a35b82911138e
joker_xjh <1781934063@qq.com>
2018-04-28 15:00:09 +0800

    Minimum ASCII Delete Sum for Two Strings

--- Algorithms/src/leetcode/DP.java
+++ Algorithms/src/leetcode/DP.java
@@ -1714,9 +1714,59 @@ public class DP {
         return false;
     }
     
+    public int longestValidParentheses(String s) {
+    	int maxLength = 0;
+    	if(s.length() < 2)
+    		return maxLength;
+    	int[] dp = new int [s.length()];
+    	for(int i=1; i<s.length(); i++) {
+    		char c = s.charAt(i);
+    		char pre = s.charAt(i-1);
+    		if(c == '(')
+    			dp[i] = 0;
+    		else {
+    			if(pre == '(' && i > 1) {
+    				dp[i] = dp[i-2] + 2;
+    			}
+    			if(pre == '(' && i == 1) {
+    				dp[i] = 2;
+    			}
+    			if(pre == ')' && i == 1) {
+    				dp[i] = 0;
+    			}
+    			if(pre == ')' && i > 1) {
+    				int j = i - dp[i-1] - 1;
+    				if(j >= 0 && s.charAt(j) == '(') {
+    					dp[i] = j-1 >= 0 ? dp[j-1] + 2 + dp[i-1] : 2 + dp[i-1];
+    				}
+    			}
+    		}
+    		maxLength = Math.max(maxLength, dp[i]);
+    	}
+    	
+    	return maxLength;
+    }
     
-    
-    
+    public int minimumDeleteSum2(String s1, String s2) {
+        int n1 = s1.length();
+        int n2 = s2.length();
+        int[][] dp = new int[n1+1][n2+1];
+        for(int i=1; i<=n1; i++)
+        	dp[i][0] = dp[i-1][0] + s1.charAt(i-1);
+        for(int i=1; i<=n2; i++)
+        	dp[0][i] = dp[0][i-1] + s2.charAt(i-1);
+        for(int i=1; i<=n1; i++) {
+        	char c1 = s1.charAt(i-1);
+        	for(int j=1; j<=n2; j++) {
+        		char c2 = s2.charAt(j-1);
+        		if(c1 == c2)
+        			dp[i][j] = dp[i-1][j-1];
+        		else
+        			dp[i][j] = Math.min(dp[i][j-1] + c2, dp[i-1][j] + c1);
+        	}
+        }
+    	return dp[n1][n2];
+    }
     
     
     
