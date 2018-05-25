0dceb7f45cdb7e9c6e6b19e7c0c43df1bd75807c
joker_xjh <1781934063@qq.com>
2018-05-25 16:39:26 +0800

    New 21 Game

--- Algorithms/src/leetcode/DP.java
+++ Algorithms/src/leetcode/DP.java
@@ -1846,6 +1846,24 @@ public class DP {
     }
     
     
+    public double new21Game(int N, int K, int W) {
+    	if (K == 0 || N >= K + W) return 1;
+        double[] dp = new double[N+1];
+        double Wsum = 1, answer = 0;
+    	dp[0] = 1;
+    	for(int i=1; i<=N; i++) {
+    		dp[i] = Wsum / W;
+    		if(i < K)
+    			Wsum += dp[i];
+    		else
+    			answer += dp[i];
+    		if(i - W >= 0)
+    			Wsum -= dp[i-W];
+    	}
+        
+    	return answer;
+    }
+    
     
     
     
