977dedeed865f78e43c7fc479edd69a5fa009175
joker_xjh <1781934063@qq.com>
2018-06-22 08:41:49 +0800

    Play with words

--- Algorithms/src/hackerrank/Medium.java
+++ Algorithms/src/hackerrank/Medium.java
@@ -3203,7 +3203,30 @@ public class Medium {
 	
 	
 	
-	
+	static int playWithWords(String s) {
+        int n = s.length();
+		int[][] dp = new int[n][n];
+		for(int i=n-1; i>=0; i--) {
+			dp[i][i] = 1;
+			char c1 = s.charAt(i);
+			for(int j=i+1; j<n; j++) {
+				char c2 = s.charAt(j);
+				if(c1 == c2) {
+					dp[i][j] = dp[i+1][j-1] + 2;
+				}
+				else {
+					dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
+				}
+			}
+		}
+		int score = 1;
+		
+		for(int i=n-2; i>0; i--) {
+			score = Math.max(score, dp[0][i] * dp[i+1][n-1]);
+		}
+		
+		return score;
+    }
 	
 	
 	
