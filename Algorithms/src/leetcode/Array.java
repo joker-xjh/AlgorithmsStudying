f494f5aaee851dfcd17bbe9483e6817aa5db47d3
joker_xjh <1781934063@qq.com>
2018-07-01 14:19:42 +0800

    Lemonade Change

--- Algorithms/src/leetcode/Array.java
+++ Algorithms/src/leetcode/Array.java
@@ -4001,7 +4001,37 @@ public class Array {
     	 
      }
      
-     
+     public boolean lemonadeChange(int[] bills) {
+    	 int five = 0, ten = 0;
+    	 for(int bill : bills) {
+    		if(bill == 5) {
+    			five++;	
+    		}
+    		else if(bill == 10) {
+    			if(five == 0)
+    				return false;
+    			five--;
+    			ten++;
+    		}
+    		else {
+    			if(ten == 0 && five == 0)
+    				return false;
+    			
+    			if(ten > 0) {
+    				if(five == 0)
+    					return false;
+    				ten--;
+    				five--;
+    			}
+    			else {
+    				if(five < 3)
+    					return false;
+    				five -=3;
+    			}
+    		}
+    	 }
+    	 return true;
+     }
      
      
      
