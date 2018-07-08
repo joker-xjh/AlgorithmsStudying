a2abda2a580678b55d7548a7cacf30ec59b93237
joker_xjh <1781934063@qq.com>
2018-07-08 12:40:35 +0800

    Transpose Matrix

--- Algorithms/src/leetcode/Array.java
+++ Algorithms/src/leetcode/Array.java
@@ -4068,7 +4068,16 @@ public class Array {
     		 array[i] = array[i] ^ 1;
      }
      
-     
+     public int[][] transpose(int[][] A) {
+         int m = A.length, n = A[0].length;
+         int[][] transpose = new int[n][m];
+         for(int j=0; j<n; j++) {
+        	 for(int i=0; i<m; i++) {
+        		 transpose[j][i] = A[i][j];
+        	 }
+         }
+         return transpose;
+     }
      
 
 
