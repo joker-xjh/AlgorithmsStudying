ecb232a32b6ce07eb27a9245049f960689b18813
joker_xjh <1781934063@qq.com>
2018-08-05 12:43:31 +0800

    Boats to Save People

--- Algorithms/src/leetcode/Array.java
+++ Algorithms/src/leetcode/Array.java
@@ -4480,7 +4480,16 @@ public class Array {
         }
         return boats;
     }
-     
+    
+	public int numRescueBoats2(int[] people, int limit) {
+		Arrays.sort(people);
+		int i, j;
+		for(i=0, j=people.length-1; i<=j; j--) {
+			if(people[i] + people[j] <= limit)
+				i++;
+		}
+		return people.length-1 - j;
+	}
      
      
      
