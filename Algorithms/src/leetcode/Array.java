6bf87f04f17367e37b306361f4403901a787e0dd
joker_xjh <1781934063@qq.com>
2018-08-11 12:24:44 +0800

    Reachable Nodes In Subdivided Graph

--- Algorithms/src/leetcode/Array.java
+++ Algorithms/src/leetcode/Array.java
@@ -4627,6 +4627,49 @@ public class Array {
 	}
 	
 	
+	public int reachableNodes(int[][] edges, int M, int N) {
+        int[][] graph = new int[N][N];
+        for(int i=0; i<N; i++) {
+        	Arrays.fill(graph[i], -1);
+        }
+        for(int[] edge : edges) {
+        	graph[edge[0]][edge[1]] = edge[2];
+        	graph[edge[1]][edge[0]] = edge[2];
+        }
+        int nodes = 0;
+		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (b[1] - a[1]));
+		boolean[] visited = new boolean[N];
+		pq.offer(new int[] {0, M});
+		while(!pq.isEmpty()) {
+			int[] cur = pq.poll();
+			int start = cur[0];
+			int moves = cur[1];
+			if(visited[start]) {
+				continue;
+			}
+			visited[start] = true;
+			nodes++;
+			for(int i=0; i<N; i++) {
+				if(graph[start][i] > -1) {
+					if(moves > graph[start][i] && !visited[i]) {
+						pq.offer(new int[] {i, moves - graph[start][i] - 1});
+					}
+					graph[i][start] -= Math.min(moves, graph[start][i]);
+                    nodes += Math.min(moves, graph[start][i]);
+				}
+			}
+		}
+		return nodes;
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
