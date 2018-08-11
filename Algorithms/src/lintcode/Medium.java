7fdd4d1d414d97e4757b7233c79451e6cf7b4e73
joker_xjh <1781934063@qq.com>
2018-08-11 14:43:58 +0800

    换硬币

--- Algorithms/src/lintcode/Medium.java
+++ Algorithms/src/lintcode/Medium.java
@@ -1679,6 +1679,47 @@ public class Medium {
 		  return answer;
 	  }
 	  
+	  public int coinChange(int[] coins, int amount) {
+		  int[][] dp = new int[coins.length+1][amount+1];
+		  for(int i=0; i<dp.length; i++) {
+			  Arrays.fill(dp[i], -1);
+		  }
+		  dp[0][0] = 0;
+		  for(int i=1; i<=coins.length; i++) {
+			  int coin = coins[i-1];
+			  dp[i][0] = 1;
+			  for(int j=1; j<=amount; j++) {
+				  for(int k=0; k*coin <= j; k++) {
+					  if(dp[i-1][j - k*coin] != -1) {
+						  if(dp[i][j] == -1)
+							  dp[i][j] = dp[i-1][j - k*coin] + k;
+						  else 
+							  dp[i][j] = Math.min(dp[i][j], dp[i-1][j - k*coin] + k);
+					  }
+				  }
+			  }
+		  }
+		  if(dp[coins.length][amount] != -1)
+			  return dp[coins.length][amount];
+		  return -1;
+	  }
+	  
+	  public int coinChange2(int[] coins, int amount) {
+		  int[] dp = new int[amount+1];
+		  Arrays.fill(dp, -1);
+		  dp[0] = 0;
+		  for(int coin : coins) {
+			  for(int i=coin; i<=amount; i++) {
+				  if(dp[i-coin] == -1)
+					  continue;
+				  if(dp[i] == -1)
+					  dp[i] = dp[i-coin] + 1;
+				  else
+					  dp[i] = Math.min(dp[i], dp[i-coin] + 1);
+			  }
+		  }
+		  return dp[amount];
+	  }
 	  
 	  
 	  
