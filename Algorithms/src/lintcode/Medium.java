8ee634d69e5d3b921c822cc647bf18c92d3ea686
joker_xjh <1781934063@qq.com>
2018-08-15 22:12:12 +0800

    加油站

--- Algorithms/src/lintcode/Medium.java
+++ Algorithms/src/lintcode/Medium.java
@@ -2239,7 +2239,22 @@ public class Medium {
 	   }
 	   
 	   
-	   
+	   public int canCompleteCircuit(int[] gas, int[] cost) {
+	        int start = 0;
+	        int sum = 0;
+	        int total = 0;
+	        for(int i=0; i<gas.length; i++) {
+	        	sum += gas[i] - cost[i];
+	        	total += gas[i] - cost[i];
+	        	if(sum < 0) {
+	        		sum = 0;
+	        		start = i+1;
+	        	}
+	        }
+	        if(total < 0)
+	        	return -1;
+	        return start;
+	    }
 	   
 	   
 	   
