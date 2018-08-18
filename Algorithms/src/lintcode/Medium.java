91df692e6aef0a5d1a161f617d2b64fb25b41260
joker_xjh <1781934063@qq.com>
2018-08-18 16:55:58 +0800

    平面范围求和 -不可变矩阵

--- Algorithms/src/lintcode/Medium.java
+++ Algorithms/src/lintcode/Medium.java
@@ -2143,33 +2143,30 @@ public class Medium {
 		   long[][] dp;
 
 		    public NumMatrix(int[][] matrix) {
-		        dp = new long[matrix.length][matrix[0].length];
-		        dp[0][0] = matrix[0][0];
-		        for(int i=1; i<matrix[0].length; i++) {
-		        	dp[0][i] += matrix[0][i-1];
+		        dp = new long[matrix.length][matrix[0].length+1];
+		        for(int i=0; i<matrix.length; i++) {
+		        	for(int j=0; j<matrix[0].length; j++) {
+		        		dp[i][j+1] = dp[i][j] + matrix[i][j];
+		        	}
 		        }
 		        for(int i=1; i<matrix.length; i++) {
 		        	for(int j=0; j<matrix[0].length; j++) {
-		        		if(j == 0) {
-		        			dp[i][j] = dp[i-1][0] + matrix[i][0];
-		        		}
-		        		else {
-		        			dp[i][j] = dp[i-1][j] + dp[i][j-1] + matrix[i][j];
-		        		}
+		        		dp[i][j+1] += dp[i-1][j+1];
 		        	}
 		        }
+		        
 		    }
 		    
 		    public int sumRegion(int row1, int col1, int row2, int col2) {
-		        long sum = dp[row2][col2];
+		        long sum = dp[row2][col2+1];
 		        if(col1 > 0) {
-		        	sum -= dp[row2][col1-1];
+		        	sum -= dp[row2][col1-1+1];
 		        }
 		        if(row1 > 0) {
-		        	sum -= dp[row1-1][col2];
+		        	sum -= dp[row1-1][col2+1];
 		        }
 		        if(row1 > 0 && col1 > 0) {
-		        	sum += dp[row1-1][col1-1];
+		        	sum += dp[row1-1][col1-1+1];
 		        }
 		    	return (int) sum;
 		    }
