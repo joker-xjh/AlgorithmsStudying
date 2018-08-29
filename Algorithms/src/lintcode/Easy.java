15626872fb54858209ccf8b8af0dec007d347a59
joker_xjh <1781934063@qq.com>
2018-08-29 21:40:53 +0800

    会议室

--- Algorithms/src/lintcode/Easy.java
+++ Algorithms/src/lintcode/Easy.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -791,7 +792,32 @@ public class Easy {
 		return true;
     }
 	
-	
+	public boolean canAttendMeetings2(List<Interval> intervals) {
+		if(intervals == null || intervals.isEmpty())
+			return true;
+		List<int[]> list = new ArrayList<>(intervals.size() * 2);
+		for(Interval interval : intervals) {
+			list.add(new int[] {interval.start, 1});
+			list.add(new int[] {interval.end, -1});
+		}
+		Collections.sort(list, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if(o1[0] == o2[0]) {
+					return o1[1] - o2[1];
+				}
+				return o1[0] - o2[0];
+			}
+		});
+		int temp = 0;
+		for(int[] array : list) {
+			temp += array[1];
+			if(temp > 1)
+				return false;
+		}
+		
+		return true;
+	}
 	
 	
 	
