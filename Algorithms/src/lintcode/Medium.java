7c4856f43e42f8d7522c7c3f9f52d3a2c641d06e
joker_xjh <1781934063@qq.com>
2018-09-15 09:40:49 +0800

    有效回文串

--- Algorithms/src/lintcode/Medium.java
+++ Algorithms/src/lintcode/Medium.java
@@ -3652,7 +3652,36 @@ public class Medium {
 		 return result;
 	 }
 	   
-	   
+	
+	 public boolean isPalindrome(String s) {
+		 int left = 0, right = s.length() - 1;
+		 while(left <= right) {
+			 char c1 = s.charAt(left);
+			 char c2 = s.charAt(right);
+			 if(!((c1 >= 'a' && c1 <= 'z') || (c1 >= 'A' && c1 <= 'Z') || (c1 >= '0' && c1 <= '9'))) {
+				 left++;
+				 continue;
+			 }
+			 if(!((c2 >= 'a' && c2 <= 'z') || (c2 >= 'A' && c2 <= 'Z') || (c2 >= '0' && c2 <= '9'))) {
+				 right--;
+				 continue;
+			 }
+			 if(c1 >= 'A' && c1 <= 'Z') {
+				 c1 += 32;
+			 }
+			 if(c2 >= 'A' && c2 <= 'Z') {
+				 c2 += 32;
+			 }
+			 if(c1 == c2) {
+				 left++;
+				 right--;
+			 }
+			 else {
+				 return false;
+			 }
+		 }
+		 return true;
+	 }
 	   
 	   
 	   
