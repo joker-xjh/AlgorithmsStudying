bcd411cb21c58ffad4b6a1d187d0acd4b201cd44
joker_xjh <1781934063@qq.com>
2018-09-29 22:08:12 +0800

    奇偶分割数组

--- Algorithms/src/lintcode/Easy.java
+++ Algorithms/src/lintcode/Easy.java
@@ -1147,7 +1147,17 @@ public class Easy {
     }
 	
 	
-	
+	public void partitionArray(int[] nums) {
+        int index = 0;
+        for(int i=0; i<nums.length; i++) {
+        	if((nums[i] & 1) == 1) {
+        		int temp = nums[index];
+        		nums[index] = nums[i];
+        		nums[i] = temp;
+        		index++;
+        	}
+        }
+    }
 	
 	
 	
