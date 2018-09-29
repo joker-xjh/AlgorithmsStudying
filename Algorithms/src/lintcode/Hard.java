d29868f47fbdb82f9f1c248229ac0f7fffd8eece
joker_xjh <1781934063@qq.com>
2018-09-30 07:49:58 +0800

    寻找重复的数

--- Algorithms/src/lintcode/Hard.java
+++ Algorithms/src/lintcode/Hard.java
@@ -759,7 +759,23 @@ public class Hard {
 	}
 	   
 	   
-	   
+	public int findDuplicate(int[] nums) {
+		int n = nums.length-1;
+		int start = 1, end = n;
+		while(start < end) {
+			int mid = (start + end) / 2;
+			int count = 0;
+			for(int num : nums) {
+				if(num <= mid)
+					count++;
+			}
+			if(count > mid)
+				end = mid;
+			else
+				start = mid + 1;
+		}
+		return end;
+    } 
 	   
 	   
 	   
