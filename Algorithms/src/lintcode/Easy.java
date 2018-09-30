1e8b69ce91b79f08115bc1a1769c7f989c2e161c
joker_xjh <1781934063@qq.com>
2018-09-30 08:30:27 +0800

    Reverse String

--- Algorithms/src/lintcode/Easy.java
+++ Algorithms/src/lintcode/Easy.java
@@ -1213,7 +1213,11 @@ public class Easy {
     }
 	
 	
-	
+	public String reverseString(String s) {
+        if(s == null || s.length() == 0)
+        	return s;
+        return new StringBuilder(s).reverse().toString();
+    }
 	
 	
 	
